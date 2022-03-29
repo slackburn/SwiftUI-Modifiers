9a05db8a68e8931f87340d891b4b111b19a16984
Samuel Blackburn <samuel.blackburn@nbrown.co.uk>
2022-03-29 15:12:38 +0100

    Initial Commit

---
+++ SwiftUI Modifiers/SwiftUI_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ModifiersApp.swift
+//  SwiftUI Modifiers
+//
+//  Created by Samuel Blackburn on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
